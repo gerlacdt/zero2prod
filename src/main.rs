9e19aad162c181832c45b67f454a3eb41e1da279
Daniel Gerlach <daniel.gerlach1981@googlemail.com>
2022-10-01 14:12:27 +0200

    added Github Action workflow

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
 fn main() {
     println!("Hello, world!");
 }
+
+// #[allow(dead_code)]
+fn sum(a: i32, b: i32) -> i32 {
+    a + b
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    #[test]
+    fn test_um() {
+        let actual = sum(1, 3);
+        let expected = 4;
+        assert_eq!(expected, actual);
+    }
+}

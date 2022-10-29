12262b56e740c7b27786da2998a6a4c35febc69c
Daniel Gerlach <daniel.gerlach1981@googlemail.com>
2022-10-29 20:52:45 +0200

    added concurrency test

--- tests/api/helpers.rs
+++ tests/api/helpers.rs
@@ -48,16 +48,6 @@ impl TestApp {
             .expect("Failed to execute request")
     }
 
-    pub async fn post_newsletters(&self, body: serde_json::Value) -> reqwest::Response {
-        self.api_client
-            .post(&format!("{}/newsletters", &self.address))
-            .basic_auth(&self.test_user.username, Some(&self.test_user.password))
-            .json(&body)
-            .send()
-            .await
-            .expect("Failed to execute request")
-    }
-
     pub fn get_confirmation_links(&self, email_request: &wiremock::Request) -> ConfirmationLinks {
         let body: serde_json::Value = serde_json::from_slice(&email_request.body).unwrap();
         // Extract the link from one of the request fields.
